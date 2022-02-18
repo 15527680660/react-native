13ea5d7078eabe270aec922c9977d311171c6dec
王进 <1251421331@qq.com>
2022-02-18 10:57:37 +0800

    react-native_toiletApp

---
+++ project/example/react-native_toiletApp-master/toilet/ios_views/toiletPage.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    WebView
+} from 'react-native';
+//引用自定义组件
+import TWebView from './twebview';
+
+//卫生间模块
+class toiletPage extends Component {
+
+    render() {
+        //单独的页面，专门处理地图相关操作
+        var cmapUri = "http://localhost:63342/toiletApp/toilet/html/nearby.html";
+        return (
+            <View style={styles.container}>
+                <TWebView url={cmapUri}/>
+            </View>
+        );
+    }
+
+}
+
+//样式表
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
+
+module.exports = toiletPage;
